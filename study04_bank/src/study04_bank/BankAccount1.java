fef36a386dead4f1016e3d0c47fd5f657880b8a8
hanyong5 <hanyong5@naver.com>
2025-02-14 12:48:40 +0900

    bank1

--- study04_bank/src/study04_bank/BankAccount1.java
+++ study04_bank/src/study04_bank/BankAccount1.java
@@ -37,11 +37,37 @@ public class BankAccount1 {
 			balance += amount;
 			transactions.add("입금 :" + amount + "원, 현재잔액 : " + balance + "원 입니다.");
 			System.out.println(amount + "원이 입금되었습니다. 현재잔액 : " + balance + "원 입니다.");
-			System.out.println();
+			System.out.println("------------------------------------------------------");
 		}else {
 			System.out.println("입금금액이 0원보다 커야합니다.");
 		}
 		
+	}
+
+
+
+
+	public void checkBalace() {
+		System.out.println(owner + "님 잔액은 " + balance + "원 입니다.");
+		System.out.println("---------------------------------------");
+	}
+
+
+
+
+	public void withDraw(double amount) {
+		if(amount > 0 && balance >= amount) {
+			balance -= amount;
+			transactions.add("출금 :" + amount + "원, 현재잔액 : " + balance + "원 입니다.");
+			System.out.println(amount + "원이 출금되었습니다. 현재잔액 : " + balance + "원 입니다.");
+			System.out.println("------------------------------------------------------");
+		}else if(amount > balance){
+			System.out.println("잔액이 부족합니다. 현재잔액은 " + balance + "원 입니다.");
+			System.out.println("------------------------------------------------------");
+		}else {
+			System.out.println("출금 금액은 0원 보다 커야합니다.");
+			System.out.println("------------------------------------------------------");
+		}
 	}
 	
 	
--- study04_bank/src/study04_bank/BankTest1.java
+++ study04_bank/src/study04_bank/BankTest1.java
@@ -3,10 +3,16 @@ package study04_bank;
 public class BankTest1 {
 
 	public static void main(String[] args) {
-		BankAccount1 account = new BankAccount1("123-456-789","홍길동",100000);
-//		BankAccount1 account1 = new BankAccount1("123-456-780","한성용",10000);
+		BankAccount1 account = new BankAccount1("123-456-789","스티브 잡스",100000);
+
 		
 		account.deposit(50000);
+		account.withDraw(10000);
+		account.withDraw(30000);
+		account.withDraw(200000);
+		
+		
+		account.checkBalace();
 		
 		
 		account.printTransactions(); // 내역서 확인
