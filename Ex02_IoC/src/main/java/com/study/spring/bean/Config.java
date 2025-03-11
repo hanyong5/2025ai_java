30ea4ec519b2899464d1b2151fdf8f1b70ee41f1
hanyong5 <hanyong5@naver.com>
2025-03-11 11:57:16 +0900

    ioc

---
+++ Ex02_IoC/src/main/java/com/study/spring/bean/Config.java
@@ -0,0 +1,20 @@
+package com.study.spring.bean;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+	
+	@Bean
+	public Member member1() {
+		Member member1 = new Member();
+		member1.setName("홍길동");
+		member1.setNickname("도사");
+		member1.setPrinter(new PrinterA());
+
+		return member1;
+	}
+
+}
