3864228f2cf070a7e6d95fe371183beed37bc8a0
hanyong5 <hanyong5@naver.com>
2025-05-29 12:23:35 +0900

    login

--- Ex10_JPA_Member/src/main/java/com/study/spring/member/service/CustomUserDetailService.java
+++ Ex10_JPA_Member/src/main/java/com/study/spring/member/service/CustomUserDetailService.java
@@ -1,19 +1,33 @@
 package com.study.spring.member.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import com.study.spring.member.entity.Member;
+import com.study.spring.member.repository.MemberRepository;
 
 import lombok.extern.log4j.Log4j2;
 
 @Service
 @Log4j2
 public class CustomUserDetailService implements UserDetailsService{
+	
+	@Autowired
+	private MemberRepository memberRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
+		log.info("**************load user by username *********************"+ username);
+		
+		Optional<Member> member = memberRepository.findByEmail(username);
+		
+		log.info("----> "+member+" <----");
+		
 		return null;
 	}
 
